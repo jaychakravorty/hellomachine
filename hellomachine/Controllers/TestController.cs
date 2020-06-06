01751fc0746c87865c15ada07dbc1a12950a88d3
DonCarnage <jayanta.chakravorty@outlook.com>
2020-06-06 17:02:10 +0530

    First commit for hello machine

---
+++ hellomachine/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hellomachine.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+           return $"Hello from {Environment.MachineName}";
+        }
+    }
+}
